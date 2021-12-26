eca776de40def74bf94ac91b1460f16090316b5a
Loïck Dumoulin <dumoulingr0eisti.eu>
2021-12-26 16:27:44 +0100

    Creation form

---
+++ demo/src/main/resources/static/script.js
@@ -0,0 +1,11 @@
+
+
+function otherGenre(){
+    a=document.getElementById('other_genre');
+    a.checked=true;
+    }
+
+function Genre(){
+    a=document.getElementById('genre');
+    a.value="";
+    }
