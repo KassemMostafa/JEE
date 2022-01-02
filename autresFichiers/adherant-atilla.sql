7ca9899ecc9a6d4ea8731c26a47953350b85f811
Mostafa Kassem <kassemmost@cy-tech.fr>
2022-01-02 22:35:22 +0100

    mysql modified

--- autresFichiers/adherant-atilla.sql
+++ autresFichiers/adherant-atilla.sql
@@ -20,19 +20,19 @@ CREATE TABLE cotisation (
 	idCotisation INT PRIMARY KEY AUTO_INCREMENT,
     idAdherant INT,
     cotisantionValide BOOL DEFAULT 0,
-    FOREIGN KEY fk_adherant(idAdherant) REFERENCES Adherant(id) ON DELETE CASCADE);
+    FOREIGN KEY fk_adherant(idAdherant) REFERENCES adherant(id) ON DELETE CASCADE);
 CREATE TABLE reponse (
 	idReponse INT PRIMARY KEY AUTO_INCREMENT,
     idAdherant INT,
     connaissancesInfo varchar(45) DEFAULT NULL,
     attentes varchar(45) DEFAULT NULL,
 	centreInterets varchar(45) DEFAULT NULL,
-    FOREIGN KEY fk_adherant(idAdherant) REFERENCES Adherant(id) ON DELETE CASCADE);
+    FOREIGN KEY fk_adherant(idAdherant) REFERENCES adherant(id) ON DELETE CASCADE);
 CREATE TABLE admin (
 	username varchar(45) PRIMARY KEY,
     password varchar(64),
     idAdherant INT,
-	 FOREIGN KEY fk_adherant(idAdherant) REFERENCES Adherant(id) ON DELETE CASCADE);
+	 FOREIGN KEY fk_adherant(idAdherant) REFERENCES adherant(id) ON DELETE CASCADE);
 
     INSERT INTO adherant Values (1, 'John', 'Doe', 'M','1999-03-30', 'ING2','Cergy', 'John@cy-tech.fr', 1, 1);
     INSERT INTO adherant Values (2, 'rené', 'provence', 'neutre','1999-03-30', 'ING2','Pau', 'rené@cy-tech.fr', 0, 0);
