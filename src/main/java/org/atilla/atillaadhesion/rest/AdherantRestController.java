e43a9c1cd45e0304bdb014dd226c0379612a9d26
Mostafa Kassem <kassemmost@cy-tech.fr>
2022-01-02 14:49:10 +0100

    Delete adherant rest API method added

--- src/main/java/org/atilla/atillaadhesion/rest/AdherantRestController.java
+++ src/main/java/org/atilla/atillaadhesion/rest/AdherantRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.atilla.atillaadhesion.entity.Adherant;
 import org.atilla.atillaadhesion.service.AdherantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +34,7 @@ public class AdherantRestController {
 	public Adherant getAdherant(@PathVariable int id) {
 		Adherant adherant = adherantService.getAdherant(id);
 		if (adherant == null) {
-			throw new RuntimeException("Adherant Not found avec id = " + id);
+			throw new RuntimeException("Adherant not found avec id = " + id);
 		}
 		return adherant;
 	}
@@ -49,6 +50,17 @@ public class AdherantRestController {
 	public Adherant updateAdherant(@RequestBody Adherant adherant) {
 		adherantService.saveAdherant(adherant);
 		return adherant;
+	}
+	
+	@DeleteMapping("/adherants/{id}")
+	public String deleteAdherant(@PathVariable int id) {
+		Adherant adherant = adherantService.getAdherant(id);
+		if (adherant == null) {
+			throw new RuntimeException("Adherant not found avec id = " + id);
+		}
+		adherantService.deleteAdherant(id);
+		
+		return "Adherant with id = " + id + " is deleted";
 	}
 	
 	
