475db4c0519c87ebbfc89fc06a793f5c53e70f09
Mostafa Kassem <kassemmost@cy-tech.fr>
2022-01-02 14:37:24 +0100

    Add 1 adherant added

--- src/main/java/org/atilla/atillaadhesion/AtillaAdhesionApplication.java
+++ src/main/java/org/atilla/atillaadhesion/AtillaAdhesionApplication.java
@@ -3,7 +3,9 @@ package org.atilla.atillaadhesion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//supprimer le exclude dès que la security est faite
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class AtillaAdhesionApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/atilla/atillaadhesion/rest/AdherantRestController.java
+++ src/main/java/org/atilla/atillaadhesion/rest/AdherantRestController.java
@@ -7,6 +7,8 @@ import org.atilla.atillaadhesion.service.AdherantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +34,13 @@ public class AdherantRestController {
 		if (adherant == null) {
 			throw new RuntimeException("Adherant Not found avec id = " + id);
 		}
+		return adherant;
+	}
+	
+	@PostMapping("/adherants")
+	public Adherant addAdherant(@RequestBody Adherant adherant) {
+		adherant.setId(0);
+		adherantService.saveAdherant(adherant);
 		return adherant;
 	}
 	
